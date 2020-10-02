7a06f355c601142b2e696e5a001b7e17c9810aee
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2020-10-02 11:36:41 +0200

    added GraphTest.determineShortestPahNO_VIA NOT FINISHED!

--- src/htbla/aud3/graphtheory/test/GraphTest.java
+++ src/htbla/aud3/graphtheory/test/GraphTest.java
@@ -1,6 +1,8 @@
 package htbla.aud3.graphtheory.test;
 
 import htbla.aud3.graphtheory.Graph;
+import htbla.aud3.graphtheory.Path;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.File;
 import java.util.Arrays;
@@ -15,12 +17,13 @@ class GraphTest {
 
     @org.junit.jupiter.api.Test
     void read() {
+        boolean oneIsNotZero = false;
         Graph graph = new Graph();
         graph.read(new File("Suchproblem.csv"));
         double[][] edgeArray = graph.edgeArray;
         assertTrue(edgeArray.length > 0);
         assertTrue(Arrays.stream(edgeArray).count() > 0);
-        Arrays.stream(edgeArray).forEach(n -> Arrays.stream(n).forEach(a -> assertTrue(a >= 0)));
+        Arrays.stream(edgeArray).forEach(n -> Arrays.stream(n).forEach(a -> {assertTrue(a >= 0);}));
 
         Graph graph2 = new Graph();
         graph2.read(new File("FLussproblem.csv"));
@@ -31,11 +34,23 @@ class GraphTest {
     }
 
     @org.junit.jupiter.api.Test
-    void determineShortestPath() {
+    void testDetermineShortestPathNO_VIA() {
+        Graph graph = new Graph();
+        graph.read(new File("Suchproblem.csv"));
+        int randomNum1 = (int)Math.random()*50;
+        int randomNum2 = (int)Math.random()*50;
+        Path path1 = graph.determineShortestPath(randomNum1,randomNum2);
+        Path path2 = graph.determineShortestPath(randomNum1,randomNum2);
+
+        if(path1 != null && path2 != null){
+            assertTrue(path1.computeDistance() == path2.computeDistance());
+        }
+        //assertTrue(path1.equals(path2));
+        throw new NotImplementedException();
     }
 
     @org.junit.jupiter.api.Test
-    void testDetermineShortestPath() {
+    void testDetermineShortestPathWITH_VIA() {
     }
 
     @org.junit.jupiter.api.Test
