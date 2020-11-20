a29dc9fcf639f1bc7a307d9821497a9335490e86
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2020-11-20 10:55:26 +0100

    fixed read

--- src/htbla/aud3/graphtheory/Graph.java
+++ src/htbla/aud3/graphtheory/Graph.java
@@ -11,15 +11,18 @@ public class Graph {
     public static double[][] edgeArray;
 
     public void read(File adjacencyMatrix) {
-        edgeArray = new double[50][50];
+//        edgeArray = new double[50][50];
         int row = 0;
         if(adjacencyMatrix.exists()){
             try {
                 BufferedReader br = new BufferedReader(new FileReader(adjacencyMatrix));
+                int num = Integer.parseInt(String.valueOf(br.lines().count()));
+                edgeArray = new double[num][num];
+
                 String line = br.readLine();
                 while(line!=null){
                     String[] parts = line.split(";");
-                    for (int i = 0; i < 50; i++){
+                    for (int i = 0; i < num; i++){
                         edgeArray[row][i] = Double.parseDouble(parts[i]);
                     }
                     line = br.readLine();
@@ -33,6 +36,8 @@ public class Graph {
     }
     
     public Path determineShortestPath(int sourceNodeId, int targetNodeId) {
+
+
         return null;
     }
     
