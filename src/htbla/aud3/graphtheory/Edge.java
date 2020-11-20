1d25b183e0b5f8956a7cdb9456b0262ebcde3eed
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2020-11-20 11:24:14 +0100

    done edge

--- src/htbla/aud3/graphtheory/Edge.java
+++ src/htbla/aud3/graphtheory/Edge.java
@@ -4,13 +4,23 @@ package htbla.aud3.graphtheory;
  * @author bheissenberger, fhainzinger, mschneglberger
  */
 public class Edge {
+    private int fromNode;
+    private int toNode;
     
     public int getFromNodeId() {
-        return -1;
+        return fromNode;
     }
     
     public int getToNodeId() {
-        return -1;
+        return toNode;
+    }
+
+    public void setFromNodeId(int id){
+        fromNode = id;
+    }
+
+    public void setToNodeId(int id){
+        toNode = id;
     }
     
 }
